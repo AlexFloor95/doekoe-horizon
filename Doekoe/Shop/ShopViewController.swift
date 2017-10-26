01016aff506221f3cff01cc126d4d5c69fcdde3d
Alex Floor <Alex.Floor@topicus.nl>
2017-10-26 19:00:26 +0200

    fonts fixed

--- Doekoe/Shop/ShopViewController.swift
+++ Doekoe/Shop/ShopViewController.swift
@@ -11,13 +11,20 @@ import UIKit
 class ShopViewController: UIViewController {
 
     @IBOutlet weak var moneyLabel: UILabel!
+    @IBOutlet weak var earningsLabel: UILabel!
+    @IBOutlet weak var spendingsLabel: UILabel!
     
     @IBOutlet weak var swipeLeftButton: UIButton!
     @IBOutlet weak var swipeRightButton: UIButton!
+    @IBOutlet weak var purchaseButton: UIButton!
+    
+    @IBOutlet weak var characterView: CharacterView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        purchaseButton.layer.cornerRadius = 3
+    characterView.initializeCharacterView(withCharacter: CharacterService.shared().getCharacter()!)
+        loadCharacterValues()
         // Do any additional setup after loading the view.
     }
 
@@ -26,5 +33,10 @@ class ShopViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func loadCharacterValues() {
+        spendingsLabel.text = "→ € " + (CharacterService.shared().getCharacter()?.costs?.description)!
+        moneyLabel.text = "€ " + (CharacterService.shared().getCharacter()?.money?.description)!
+        earningsLabel.text = "← € " + (CharacterService.shared().getCharacter()?.earnings?.description)!
+    }
 
 }
